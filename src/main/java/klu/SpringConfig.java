da2f645a05bcd5baa42633ac38908a9971b0233d
AmruthaNukala <2200031126@kluniversity.in>
2024-12-06 23:42:44 +0530

    first commit

---
+++ src/main/java/klu/SpringConfig.java
@@ -0,0 +1,14 @@
+package klu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("klu")
+@EnableWebMvc
+public class SpringConfig  implements WebMvcConfigurer{
+	
+
+}
